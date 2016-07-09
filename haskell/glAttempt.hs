4c346da96966f638def3193f8fcddf0c9011cdf7
John Grey <jgrey@ucsc.edu>
2016-07-08 19:08:56 -0700

    a working, simple, GL example

---
+++ haskell/glAttempt.hs
@@ -0,0 +1,19 @@
+import Graphics.UI.GLUT
+
+myPoints :: [(GLfloat, GLfloat, GLfloat)]
+myPoints = [ (sin (2 * pi * k/12), cos (2 * pi * k/12), 0) | k <- [1..12]]
+
+
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "Hello world"
+  displayCallback $= display
+  mainLoop
+
+display :: DisplayCallback
+display = do
+  clear [ ColorBuffer ]
+  renderPrimitive Triangles $
+    mapM_ (\(x,y,z)-> vertex $ Vertex3 x y z) myPoints
+  flush
