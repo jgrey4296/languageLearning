71b60106f484b28634bc031ad615ae688d9f94b1
John Grey <jgrey@ucsc.edu>
2016-07-08 19:07:24 -0700

    a simple means of deading files

---
+++ haskell/fileReader.hs
@@ -0,0 +1,18 @@
+-- read a file, save output to a new file
+
+import System.Environment (getArgs)
+import WC (myFunction)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: two arguments needed"
+
+
+
