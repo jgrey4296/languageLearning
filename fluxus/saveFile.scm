acec2743341d39ce26c1834be819369cfa9cd472
John Grey <jgrey@ucsc.edu>
2016-12-12 02:55:54 +0000

    simple script to save a screen image from fluxus

---
+++ fluxus/saveFile.scm
@@ -0,0 +1,11 @@
+;; A Simple remind of how to take a screenshot
+;; Probably best to Ctrl-h first
+;; saves to the Fluxus.app/Contents/Resources directory
+
+(let* ( ;; vars
+       (filename "myscreenshot.jpg")
+       )
+
+  (framedump filename)
+
+  )
