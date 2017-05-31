20cc97e9bb7ddd2f196dc07a35a7e411ed2ab32b
John Grey <jgrey@ucsc.edu>
2017-05-31 15:20:59 +0100

    updated gitignore, chained two renders in cairo haskell

--- haskell/cairoTest.hs
+++ haskell/cairoTest.hs
@@ -24,31 +24,43 @@ main = do
   widgetModifyBg canvas StateNormal (Color 65535 65535 65535)
 
   -- Listeners
-  on canvas draw $ myDraw
+  on canvas draw $ mainDraw
   on window objectDestroy mainQuit
   window `on` keyPressEvent $ tryEvent $ do
     keyName <- eventKeyName
     case (unpack keyName) of
       "q" -> liftIO $ mainQuit
-      "s" -> liftIO $ writePng "test.png" myDraw
+      "s" -> liftIO $ writePng "test.png" mainDraw
       _ -> liftIO $ putStrLn $ "Unrecognised: " ++ unpack keyName
 
   --Display:
   widgetShowAll window
   mainGUI
 
+mainDraw :: Render () 
+mainDraw = myDraw >> myDraw2
+
 
 myDraw :: Render ()
 myDraw = do
   setSourceRGB 1 1 0
   setLineWidth 5
-  
   moveTo 120 60
   lineTo 60 110
   lineTo 180 110
   closePath
-
   stroke
+
+myDraw2 :: Render ()
+myDraw2 = do
+  setSourceRGB 0 1 0
+  setLineWidth 10
+  moveTo 600 500
+  lineTo 400 400
+  lineTo 200 400
+  lineTo 500 300
+  closePath
+  fill
 
 
 writePng :: String -> Render () -> IO ()
