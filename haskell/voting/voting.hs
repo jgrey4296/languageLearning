9a3abff48f7b3525f9ba87551391bdd90b88e3de
John Grey <jgrey@ucsc.edu>
2017-06-26 16:11:32 +0100

    rewrote voting sim, added instant runoff and bordaCount election types

--- haskell/voting/voting.hs
+++ haskell/voting/voting.hs
@@ -7,13 +7,14 @@ import System.Random
 import qualified Data.List as L
 import Data.Monoid
 import qualified Data.Text as T
+import qualified Data.Map as M
 
 type Position = [Float]
-type Tally = (Position, Int)
-type VoteAggregation = [Tally]
+type Tally = (Position, Int) -- todo: switch from int count to [Int] to allow for preferences
+type Rank = (Position, [Int])
+type VoteAggregation = M.Map Position Int
 type RandMonad = StateT StdGen IO
 type ElectionM = StateT VoteAggregation RandMonad
-
 
 --Without the sqrt when just comparing
 cDistance :: Position -> Position -> Float
@@ -22,46 +23,104 @@ cDistance as bs = result
 
 distance x = sqrt . cDistance x
 
-nearestTo :: Position -> [Position] -> Int
-nearestTo pos options = result
+-- returns a sorted low->high ranking of positions
+rankDistances :: Position -> [Position] -> [Position]
+rankDistances pos options = result
   where distances = fmap (\x -> cDistance pos x) options
-        result = snd . head . L.sort $ zip distances [1..]
+        result = fmap snd $ L.sort $ zip distances options
 
 
-simpleElection :: Int -> Int -> Int -> ElectionM VoteAggregation
-simpleElection numVoters numReps vectorSize = do
-  reps <- lift $ replicateM numReps $ createVector vectorSize
-  voters <- lift $ replicateM numVoters $ createVector vectorSize
-  sortVotes reps voters
+-- ##############################
+-- ELECTION TYPES
+-- ##############################
+
+firstPastThePost :: Int -> Int -> Int -> ElectionM VoteAggregation
+firstPastThePost numVoters numReps vectorSize = do
+  (reps, voters) <- electionSetup numVoters numReps vectorSize
+  -- assign votes by similarity to reps
+  let allRanked = fmap (flip rankDistances reps) voters
+      topVotes = fmap head allRanked
+  tallyVotes reps topVotes
   voteTallies <- get
   return voteTallies
 
-sortVotes :: [Position] -> [Position] -> ElectionM ()
-sortVotes reps voters = do
-  --initialise
-  put $ zip reps $ take (length reps) $ repeat 0
-  let incVoteBound = incVote reps
-  mapM_ (\x -> incVoteBound x) voters
+instantRunoff :: Int -> Int -> Int -> Int -> ElectionM VoteAggregation
+instantRunoff numRankings numVoters numReps vectorSize = do
+  -- setup
+  (reps, voters) <- electionSetup numVoters numReps vectorSize
+  -- rank
+  let allRanked = fmap (flip rankDistances reps) voters
+  -- runoff
+  recursiveRunoff reps allRanked (numVoters `div` 2)
+  voteTallies <- get
+  return voteTallies
+
+-- Calc first place, if enough, end. otherwise, recurse
+recursiveRunoff :: [Position] -> [[Position]] -> Int -> ElectionM ()
+recursiveRunoff reps ranks minVotes = do
+  liftIO $ print $ "Running off: " ++ show (length ranks)
+  tallyVotes reps $ fmap head ranks
+  results <- get
+  liftIO $ printResults results
+
+  winnerAmnt <- gets maximum
+  liftIO $ print $ "Winner amnt: " ++ show winnerAmnt
+  if winnerAmnt >= minVotes
+    then return ()
+    else setupRecurse reps ranks minVotes
+
+-- Calc last place, remove, run next runoff
+setupRecurse :: [Position] -> [[Position]] -> Int -> ElectionM ()
+setupRecurse reps ranks minVotes = do
+  tallyVotes reps $ fmap last ranks
+  minResults <- get
+  let (minPos, minAmnt) = getCmpFromMap (\x y -> x < y && x > 0) minResults
+      ranks' = fmap (filter (\y -> y /= minPos)) ranks
+      ranks'' = filter (\x -> 0 < length x) ranks'
+  recursiveRunoff reps ranks'' minVotes
+
+
+bordaCount :: Int -> Int -> Int -> ElectionM VoteAggregation
+bordaCount numVoters numReps vectorSize = do
+    (reps, voters) <- electionSetup numVoters numReps vectorSize
+    let allRanked = fmap (flip rankDistances reps) voters
+        voteTuples = fmap (flip zip [1..]) allRanked
+    tallyVoteTuples reps voteTuples
+    voteTallies <- get
+    return voteTallies
+
+-- Utilities:
+
+
+--Init the reps, voters for an election
+electionSetup :: Int -> Int -> Int -> ElectionM ([Position], [Position])
+electionSetup numVoters numReps vectorSize = do
+  reps <- lift $ replicateM numReps $ createVector vectorSize
+  voters <- lift $ replicateM numVoters $ createVector vectorSize
+  return (reps, voters)
   
-incVote :: [Position] -> Position -> ElectionM ()
-incVote positions voter = do
-  let votedFor = nearestTo voter positions
-  modify $ incrementForI votedFor 1
-  
-incrementForI :: Int -> Int -> VoteAggregation -> VoteAggregation
-incrementForI pos amnt aggregate = result
-  where initial | pos == 0 = []
-                | pos > 0 = take (pos - 1) aggregate
-        (patt, count) = head . drop (pos - 1) $ aggregate
-        rest = drop pos aggregate
-        result = initial ++ [(patt, count + amnt)] ++ rest
+--For each rep, count the votes for it
+tallyVotes :: [Position] -> [Position] -> ElectionM ()
+tallyVotes reps votes = do
+  put $ M.fromList $ zip reps $ repeat 0
+  mapM_ (\x -> incVote x 1) votes
+
+tallyVoteTuples :: [Position] -> [[(Position, Int)]] -> ElectionM ()
+tallyVoteTuples reps votes = do
+  put $ M.fromList $ zip reps $ repeat 0
+  mapM_ (\rankings -> mapM_ (\(pos, amnt) -> incVote pos amnt) rankings) votes
+
+incVote :: Position -> Int -> ElectionM ()
+incVote vote amnt  = do
+  modify $ M.adjust (+ amnt) vote
 
 printResults :: VoteAggregation -> IO ()
 printResults results = do
   let alphabet = fmap T.singleton ['a'..'z']
       addSuffixes xs n = fmap (\x -> x <> (T.pack $ show n)) xs <> addSuffixes xs (n+1)
       infiniteSupply = addSuffixes alphabet (0 :: Integer)
-      zipped = fmap (\(s, (_, c)) -> (c, s)) $ L.zip infiniteSupply results
+      --
+      zipped = fmap (\(s, (_, c)) -> (c, s)) $ L.zip infiniteSupply $ M.assocs results
   forM_ (reverse . L.sort $  zipped) (\(c, s) -> putStrLn $ T.unpack $ s <> (T.pack ": ") <> (T.pack $ show c))
 
 createVector :: Int -> RandMonad Position
@@ -77,15 +136,27 @@ splitGen = do
   put gen''
   return gen'
 
-runElection :: Int -> Int -> Int -> IO ()
-runElection numVoters numReps vecSize = do
+getCmpFromMap :: (b -> b -> Bool) -> M.Map a b -> (a,b)
+getCmpFromMap cmp map = result
+  where aTuple = M.findMax map
+        foldFunc = (\o@(kx, cx) k c -> if cmp c cx then (k, c) else o)
+        result = M.foldlWithKey foldFunc aTuple map
+
+
+runElection :: Int -> Int -> Int -> Int -> ElectionM VoteAggregation -> IO ()
+runElection numVoters numReps vecSize seed m = do
   print "Starting Election"
   print $ "Voters : " ++ (show numVoters) ++ " Reps: " ++ (show numReps)
   print $ "Vector Size: " ++ show vecSize
-  result <- evalStateT (evalStateT (simpleElection numVoters numReps vecSize) []) (mkStdGen 235342)
-  if length result /= numReps
-    then error "Result Mismatch"
-    else printResults result
+  result <- evalStateT (evalStateT m M.empty) (mkStdGen seed)
+  print $ "Result Size: " ++  show (length result)
+  printResults result
   print "Finished"
 
-main = runElection 1000 20 10
+runFPTP = runElection 1000 20 10 23512122 (firstPastThePost 1000 20 10)
+
+runIRO = runElection 1000 20 10 23512122 (instantRunoff 4 1000 20 10) 
+
+runBorda = runElection 1000 20 10 23512122 (bordaCount 1000 20 10)
+
+main = runFPTP
