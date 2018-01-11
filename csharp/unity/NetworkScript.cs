9cb1635ee1f111204fae6008984c220537604f6a
John Grey <jgrey@ucsc.edu>
2018-01-11 03:37:47 +0000

    updated python - unity networking code to be cleaner

--- csharp/unity/NetworkScript.cs
+++ csharp/unity/NetworkScript.cs
@@ -9,40 +9,57 @@ using System.Net.Sockets;
 using System.Threading;
 using UnityEngine.Serialization;
 using System.Text;
+using System.Collections.Generic;
+using System.IO;
 
 //Data Class used to serialze to json
+//TODO: Determine final format
 [Serializable]
-public class MyData {
+public class VaultData {
+	public enum NetworkMessageT { S_HANDSHAKE, C_HANDSHAKE, INFO, ACTION, AI_GO, AI_COMPLETE, QUIT };
 
-	public int age;
-	public string name;
+	public int size;
+	public NetworkMessageT iden;
+	public string data;
 
+	public VaultData(int s, NetworkMessageT t, string d){
+		this.size = s;
+		this.iden = t;
+		this.data = d;
+	}
 }
-
 
 public class NetworkScript : MonoBehaviour
 {
+	//Target:
 	public String host = "localhost";
 	public Int32 port = 50000;
 
+	//Internal Management
 	Boolean connected = false;
 	internal Boolean socket_ready = false;
 	internal String input_buffer = "";
 
+	//Actual Network connections
 	TcpClient tcp_socket;
 	NetworkStream net_stream;
-
 	StreamWriter socket_writer;
 	StreamReader socket_reader;
 
-	//Data Object for sending and receiving
-	MyData theData = new MyData();
-	MyData returnData = new MyData();
+	//Data Objects for sending and receiving
+	Queue<VaultData> toServerQueue = new Queue<VaultData>();
+	Queue<VaultData> fromServerQueue = new Queue<VaultData> ();
+	//Raw text data to be sent
     string dataAsJson = "";
-    string receivedData = "";
+    string receivedJson = "";
     
+	/*
+	 * Upon Creation of the network script,
+	 * setup the network.
+	 */
 	private void Start() {
 		Debug.Log ("Starting server");
+		Debug.Log ("Enum Values: " + (int) VaultData.NetworkMessageT.S_HANDSHAKE);
         try	{
 			tcp_socket = new TcpClient(host, port);
 			net_stream = tcp_socket.GetStream();
@@ -51,10 +68,9 @@ public class NetworkScript : MonoBehaviour
 			socket_ready = true;
 			socket_writer.AutoFlush = true;
 			connected = true;
-            //Send an initial bit of data:
-			theData.age = 10;
-			theData.name = "Handshake";
-			writeSocket (theData);
+            //SETUP HANDSHAKE:
+			toServerQueue.Enqueue(new VaultData(0, VaultData.NetworkMessageT.C_HANDSHAKE, "initialise"));
+			writeSocket ();
 		}
 		catch (Exception e)	{
 			// Something went wrong
@@ -63,8 +79,10 @@ public class NetworkScript : MonoBehaviour
 
 	}
 
-	//... writing to a socket...
-	public void writeSocket(MyData d) {
+	/*
+	 * Given some data, serialize it and write out
+	 */
+	public void writeSocket() {
 		if (!socket_ready) {
 			Debug.Log ("Early Exit in writeSocket");
 			return;
@@ -73,41 +91,57 @@ public class NetworkScript : MonoBehaviour
 			Debug.Log ("Can't Write");
 			return;
 		}
-        dataAsJson = JsonUtility.ToJson (d);		
-		socket_writer.Write(dataAsJson);
+		foreach (var datum in this.toServerQueue) {
+			dataAsJson = JsonUtility.ToJson (datum);
+			Debug.Log ("Sending: " + dataAsJson);
+			socket_writer.Write (dataAsJson);
+		}
+		socket_writer.Flush ();
+		this.toServerQueue.Clear ();
 	}
 		
+	/*
+	 * The update tick where data is listened for
+	 */
 	void Update() {
 		if (!connected) {
 			return;
 		}
 
-		receivedData = readSocket();
-		if (receivedData == "") {
+		receivedJson = readSocket();
+		if (receivedJson == "") {
 			return;
 		}
 
-		Debug.Log ("Received data: " + receivedData);
-		JsonUtility.FromJsonOverwrite (receivedData, returnData);
+		Debug.Log ("Received data: " + receivedJson);
+		//Convert json into object
+		VaultData fromNetworkData = JsonUtility.FromJson<VaultData>(receivedJson);
         //Handle the data:        
-		switch (returnData.name) {
-		case "blah":
-			theData.name = "ping";
-			writeSocket(theData);
+		switch (fromNetworkData.iden) {
+		case VaultData.NetworkMessageT.S_HANDSHAKE:
+			Debug.Log ("Network Handshake Complete");
 			break;
-
-		case "bloo":
-			theData.name = "end";
-			writeSocket (theData);
+		case VaultData.NetworkMessageT.ACTION:
+			this.fromServerQueue.Enqueue (fromNetworkData);
 			break;
-
+		case VaultData.NetworkMessageT.AI_COMPLETE:
+			Debug.Log ("AI Finished, time to trigger actions");
+			break;
+		case VaultData.NetworkMessageT.C_HANDSHAKE:
+		case VaultData.NetworkMessageT.INFO:
+		case VaultData.NetworkMessageT.AI_GO:
+			throw new Exception ();
+			break;
+		case VaultData.NetworkMessageT.QUIT:
 		default:
 			closeSocket ();
 			break;
 		}
 	}
 
-	//... reading from a socket...
+	/*
+	 * Listen to the socket, and consume information from it
+	 */
 	public String readSocket() {
 		if (!socket_ready) {
 			return "";
@@ -125,18 +159,25 @@ public class NetworkScript : MonoBehaviour
         return read;
 	}
 
+	/*
+	 * Cleanup the socket on exit
+	 */
 	void OnApplicationQuit() {
 		closeSocket();
 	}
 
-	//... closing a socket...
+	/*
+	 * Routine to close the socket, 
+	 * sending a closing handshake.
+	 */
 	public void closeSocket() {
 		Debug.Log ("Closing Socket");
 		if (!socket_ready)
 			return;
         //Send a final message before closing the socket
-		theData.name = "end";
-		writeSocket (theData);
+		toServerQueue.Clear();
+		toServerQueue.Enqueue (new VaultData (0, VaultData.NetworkMessageT.QUIT, ""));
+		writeSocket ();
 		socket_writer.Close();
 		socket_reader.Close();
 		tcp_socket.Close();
--- csharp/unity/network.py
+++ csharp/unity/network.py
@@ -1,112 +1,174 @@
 import logging as root_logger
 import socket
 import json
+from enum import Enum
 from json.decoder import JSONDecodeError
 import IPython
 
-# Setup root_logger:
-LOGLEVEL = root_logger.DEBUG
-LOG_FILE_NAME = "log.network"
-root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
-console = root_logger.StreamHandler()
-console.setLevel(root_logger.INFO)
-root_logger.getLogger('').addHandler(console)
+import logging as root_logger
 logging = root_logger.getLogger(__name__)
+####################
+DEFAULT_PORT = 50000
+DEFAULT_BLOCKSIZE = 1024
+DEFAULT_BACKLOG = 5
+DEFAULT_HOST = "localhost"
 
-#Constants:
-host = 'localhost' 
-port = 50000
-backlog = 5 
-blockSize = 1024
-theSocket  = None
 
-#Functions:
-def setup():
-    logging.info("Setting up")
-    global theSocket
-    theSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    theSocket.bind((host,port)) 
-    theSocket.listen(backlog)
+class UnityServer:
+    """ A Server to connect to unity """
+    
+    #The types of messages supported
+    MESSAGE_T = Enum("Message Types", "S_HANDSHAKE C_HANDSHAKE INFO ACTION AI_GO AI_COMPLETE QUIT", start=0)
 
-def listen():
-    logging.info("Listening")
-    global theSocket
-    if theSocket is None:
-        logging.exception("The Socket is Not Set Up")
-    try:
-        listen_loop = True
-        while listen_loop:
-            client, address = theSocket.accept()
-            client_loop = True
-            logging.info("Client connected on: {}.".format(str(address)))
-            while client_loop:
-                logging.info("--------------------")
-                logging.info('Waiting for data')
-                data = client.recv(blockSize).decode()
-                if data == "":
-                    logging.info("Nothing Received")
-                    continue
-                logging.info('Data: {}'.format(str(data)))
-                try:
-                    decoded = json.loads(data);
-                    client_loop, response = handleJson(decoded)
-                    logging.info("Continue: {}, Response: {}".format(client_loop, response))
-                    if response is not None:
-                        respond(client, response)
-                except JSONDecodeError:
-                    logging.info("Problem Decoding")
-                except TypeError as e:
-                    logging.warning("Problem Encoding")
-                    logging.warning("{}".format(str(e)))
+    def __init__(self,
+                 port=DEFAULT_PORT,
+                 host=DEFAULT_HOST,
+                 backlog=DEFAULT_BACKLOG,
+                 blockSize=DEFAULT_BLOCKSIZE):
+        self.theSocket  = None
+        self.host = host
+        self.port = port
+        self.backlog = backlog 
+        self.blockSize = blockSize
+
+        self.fromClientMessages = []
+        self.toClientMessages = []
+        self.data_segments = []
+        self.data_size = 0
+
+        self.accepted_client = None
+        self.listen_for_clients = True
+        self.listen_for_data = True
+    
+    #Methods:
+    def setup(self):
+        logging.info("Setting up Python Server")
+        self.theSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.theSocket.bind((self.host, self.port)) 
+        self.theSocket.listen(self.backlog)
+
+    def close(self):
+        logging.info("Closing Socket")
+        if self.accepted_client is not None:
+            self.accepted_client.close()
+        if self.theSocket is not None:
+            self.theSocket.close()
+        self.listen_for_clients = False
+        self.listen_for_data = False
+
+        
+    def listen(self):
+        logging.info("Listening")
+        if self.theSocket is None:
+            raise Exception("Socket is not setup")
+        try:
+            while self.listen_for_clients:
+                #Loop for a connection
+                client, address = self.theSocket.accept()
+                self.accepted_client = client
+                self.listen_for_data = True
+                logging.info("Client connected on: {}.".format(str(address)))
+                while self.listen_for_data:
+                    #loop to actually recieve data
+                    logging.info("--------------------")
+                    logging.info('Waiting for data')
+                    data = self.accepted_client.recv(self.blockSize).decode()
+                    if not bool(data):
+                        logging.info("Nothing Received")
+                        IPython.embed(simple_prompt=True)
+                        continue
+                    logging.info('Data: {}'.format(str(data)))
+                    try:
+                        self.consume_data_segment(data)
+                    except JSONDecodeError:
+                        logging.info("Problem Decoding")
+                    except TypeError as e:
+                        logging.warning("Problem Encoding")
+                        logging.warning("{}".format(str(e)))
+                    #Data has been recieved and added, now do something with it
+                    if not bool(self.data_size):
+                        self.consume_data()
+                        
             #Finished looping with this client:
             logging.info("Closing Client")
             client.close()
+        finally:
+            #finished listening entirely
+            logging.info("Finishing Listening")
+            self.theSocket.close()
+            self.theSocket = None
+
+    def consume_data_segment(self, data):
+        """ consume and add a segment of data """
+        self.data_segments.append(data)
+            
+    def consume_data(self):
+        """ Consume and handle an entire message header """
+        data = "".join(self.data_segments)
+        self.data_segments.clear()
+        if not bool(data):
+            return
+        try:
+            decoded = json.loads(data.strip());
+        except json.decoder.JSONDecodeError:
+            IPython.embed(simple_prompt=True)
+        response = self.handleJson(decoded)
+        logging.info("Continue: {}, Response: {}".format(self.listen_for_data, response))
+        if response is not None:
+            self.respond(response)
+
+            
+    def handleJson(self, data):
+        logging.info("Handling Data: {}".format(str(data)))
+        response = None
+        if not all([x in data for x in ["size", "iden", "data"]]):
+            logging.warning("Data is malformed")
+            self.listen_for_data = False
+        elif data['iden'] == UnityServer.MESSAGE_T.C_HANDSHAKE.value:
+            logging.info('Startup')
+            response = data.copy()
+            response['iden'] = UnityServer.MESSAGE_T.S_HANDSHAKE.value
+        elif data['iden'] == UnityServer.MESSAGE_T.QUIT.value:
+            logging.info('Quitting')
+            self.close()
+        else:
+            logging.info("Other: {}".format(data['iden']))
+            self.listen_for_data = False
+
+        return response
+
+    def respond(self, data):
+        logging.info("Responding: {}".format(data))
+        if data is None:
+            raise Exception("Response Data shouldn't be None")
         
-    finally:
-        #finished listening entirely
-        logging.info("Finishing Listening")
-        theSocket.close()
-        theSocket = None
-    
-def handleJson(data):
-    logging.info("Handling Data: {}".format(str(data)))
-    cont = True
-    response = None
-    if not 'name' in data:
-        logging.warning("Data lacks a name")
-        cont = False
-        return (cont, response)
-    if data['name'] == "Handshake":
-        logging.info('Startup')
-        response = data.copy()
-        response['name'] = "blah"
-    elif data['name'] == "ping":
-        response = data.copy()
-        response['name'] = "blah"
-    elif data['name'] == "end":
-        cont = False
-    else:
-        logging.info("Closing")
-        cont = False
-    return (cont, response)
+        if not isinstance(data, str):
+            data = json.dumps(data)
+            #todo: change this to use the same byte array each time, without creating a new object
+            byteData = bytearray(data + "\n", 'utf-8')
+            self.accepted_client.sendall(byteData)
 
-def respond(client, data):
-    logging.info("Responding")
-    if data is None:
-        return
-    if not isinstance(data, str):
-        data = json.dumps(data)
-    #todo: change this to use the same byte array each time, without creating a new object
-    byteData = bytearray(data + "\n", 'utf-8')
-    client.sendall(byteData)
 
+            
 if __name__ == "__main__":
+    # Setup root_logger:
+    import logging as root_logger
+    LOGLEVEL = root_logger.DEBUG
+    LOG_FILE_NAME = "log.Network"
+    root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+    
+    console = root_logger.StreamHandler()
+    console.setLevel(root_logger.INFO)
+    root_logger.getLogger('').addHandler(console)
+    logging = root_logger.getLogger(__name__)
+    ##############################
+    logging.info("Setting up Server")
+    sev = UnityServer()
     try:
-        setup()
-        listen()
+        sev.setup()
+        sev.listen()
     except (KeyboardInterrupt):
         logging.info("Shutting down")
-        if theSocket is not None:
-            theSocket.close()
+        sev.close()
 
     
