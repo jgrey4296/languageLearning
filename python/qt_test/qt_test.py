48da21e4381b74b93653c451a18432ba6502e51c
John Grey <jgrey@ucsc.edu>
2019-04-25 09:06:34 +0100

    adds a basic qt example

---
+++ python/qt_test/qt_test.py
@@ -0,0 +1,51 @@
+"""
+The Basic python QT example from:
+https://doc.qt.io/qtforpython/gettingstarted.html
+"""
+# Setup root_logger:
+from os.path import splitext, split
+import logging as root_logger
+LOGLEVEL = root_logger.DEBUG
+LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
+root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+
+console = root_logger.StreamHandler()
+console.setLevel(root_logger.INFO)
+root_logger.getLogger('').addHandler(console)
+logging = root_logger.getLogger(__name__)
+##############################
+
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["bloo bloo bloo", "blah blah blah"]
+
+        self.button = QtWidgets.QPushButton("Click Me")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+        self.repaint()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
