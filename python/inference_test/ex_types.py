445661f48859dec3242576cd7a781890ff7ff079
John Grey <jgrey@ucsc.edu>
2019-06-23 14:00:30 +0100

    updates types to build type_declarations

--- python/inference_test/ex_types.py
+++ python/inference_test/ex_types.py
@@ -1,4 +1,5 @@
 import logging as root_logger
+import IPython
 logging = root_logger.getLogger(__name__)
 
 
@@ -12,7 +13,7 @@ class Type:
 class TypeDefinition(Type):
     """ Can define Structure of a type """
 
-    def __init__(self, name, path, structure):
+    def __init__(self, name, path, structure, tvars):
 
         """ Structure creates the dict of locations.
         Only leaves get type anotations. Thus:
@@ -22,19 +23,22 @@ class TypeDefinition(Type):
         self.name = name
         self.path = path
         self.structure = structure
+        self._vars = []
+        if tvars is not None:
+            self._vars += tvars
 
     def __repr__(self):
         return "(Data: {})".format(self.name)
 
     def build_type_declaration(self):
-        return MonoTypeVar(self.name, self.path)
+        return MonoTypeVar(self.name, self.path, self._vars[:])
 
 
 class MonoTypeVar(Type):
     """ A MonoType Instance """
     TypeCounter = 0
 
-    def __init__(self, name=None, path=None):
+    def __init__(self, name=None, path=None, args=None):
         #todo:
         if name is None:
             letter = ascii_lowercase[MonoTypeVar.TypeCounter % len(ascii_lowercase)]
@@ -43,14 +47,35 @@ class MonoTypeVar(Type):
             MonoTypeVar.TypeCounter += 1
         if path is None:
             path = name
+        if args is None:
+            args = []
         self.name = name
         self.path = path
+        self._args = args
+
+    def build_type_declaration(self, the_dict):
+        if str(self) in the_dict:
+            new_type = the_dict[str(self)]
+            return MonoTypeVar(new_type.name,
+                               new_type.path,
+                               new_type._args)
+
+        new_args = [the_dict[str(x)] if str(x) in the_dict else x for x in self._args]
+        return MonoTypeVar(self.name,
+                           self.path,
+                           new_args)
 
     def __hash__(self):
         return hash("".join([str(x) for x in self.path]))
 
     def __repr__(self):
-        return "(:: {})".format(self.name)
+        args = ""
+        if self._args:
+            args = "({})".format(", ".join(repr(x) for x in self._args))
+        return "(::{}{})".format(self.name, args)
+
+    def __str__(self):
+        return self.name.name
 
     def __eq__(self, other):
         #todo: match inheritance
@@ -58,7 +83,8 @@ class MonoTypeVar(Type):
             return False
         type_match = type(self) == type(other)
         name_match = self.name == other.name
-        return type_match and name_match
+        args_match = all([a == b for a,b in zip(self._args, other._args)])
+        return type_match and name_match and args_match
 
     def __lt__(self, other):
         raise Exception("To be implemented")
