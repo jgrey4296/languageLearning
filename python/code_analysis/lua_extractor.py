fe1bc2850b20caa47e1753291bd5b4dc432823c5
John Grey <johngrey4296@gmail.com>
2020-01-12 01:38:54 +0000

    implement basic lua extraction

--- python/code_analysis/lua_extractor.py
+++ python/code_analysis/lua_extractor.py
@@ -4,6 +4,7 @@ Get lua files from data dir,
 output to similarly named files in analysis directory
 """
 import IPython
+import utils
 from enum import Enum
 from os.path import join, isfile, exists, abspath
 from os.path import split, isdir, splitext, expanduser
@@ -23,7 +24,47 @@ console.setLevel(root_logger.INFO)
 root_logger.getLogger('').addHandler(console)
 logging = root_logger.getLogger(__name__)
 ##############################
-# Enums:
+
+lua_e = Enum('Lua Enums','COMMENT')
+
+class LuaClass(utils.ParseBase):
+
+    def __init__(self, name, parent=None):
+        super().__init__()
+        self._type = "Class"
+        self._name = name
+        self._parent = parent
+
+    def __str__(self):
+        parent = ""
+        if self._parent is not None:
+            parent = self._parent
+
+        return "{} : {} : {} : {}".format(self._line_no,
+                                          self._type,
+                                          self._name,
+                                          self._parent)
+
+class LuaFn(utils.ParseBase):
+
+    def __init__(self, name):
+        super().__init__()
+        self._type = "Function"
+        self._name = name
+
+class LuaRecipe(utils.ParseBase):
+
+    def __init__(self, name, rest):
+        super().__init__()
+        self._type = "Recipe"
+        self._name = name
+        self._rest = rest
+
+    def __str__(self):
+        return "{} : {} : {} : {}".format(self._line_no,
+                                          self._type,
+                                          self._name,
+                                          self._rest)
 
 
 
@@ -32,7 +73,7 @@ def build_parser():
     s = pp.Suppress
     op = pp.Optional
     lineEnd = pp.lineEnd
-    NAME = pp.Word(pp.alphanums + "_")
+    NAME = pp.Word(pp.alphanums + ":_.")
     NUM = pp.Word(pp.nums + ".")
     EQUAL = s(pp.Literal('='))
     COLON = pp.Literal(':')
@@ -43,24 +84,46 @@ def build_parser():
     LOCAL = pp.Keyword("local")
     OPAR = s(pp.Literal('('))
     CPAR = s(pp.Literal(')'))
+    RECIPE = s(pp.Keyword('Recipe'))
+    COMMA = s(pp.Literal(','))
 
-    class_p = NAME + EQUAL + CLS + OPAR + pp.Or([FN, NAME])
-    function_p = FN + NAME
+
+    class_p = NAME.setResultsName('name') + EQUAL + CLS + OPAR + pp.Or([FN, NAME.setResultsName('parent')])
+    function_p = op(LOCAL) + FN + NAME.setResultsName('name')
+    recipe_p = RECIPE + OPAR + pp.quotedString.setResultsName('name') \
+        + COMMA + pp.restOfLine.setResultsName('rest')
 
     #addTask
+    #states, events
 
+    #TODO:Setup parse results
+    class_p.setParseAction(lambda x: LuaClass(x.name, x.parent))
+    function_p.setParseAction(lambda x: LuaFn(x.name))
+    recipe_p.setParseAction(lambda x: LuaRecipe(x.name, x.rest))
 
-    com_parser = None
-    main_parser = None
+    com_open = pp.Literal('--')
+    com_parser = s(com_open) + pp.restOfLine
+    com_parser.setParseAction(lambda x: lua_e.COMMENT)
 
-    return main_paser, com_parser
+    main_parser = pp.MatchFirst([com_parser,
+                                 class_p,
+                                 function_p,
+                                 recipe_p,
+                                 pp.restOfLine])
+    return main_parser
 
-def extract_from_file(filename, main_parser, com_parser):
+def extract_from_file(filename, main_parser):
     logging.info("Extracting from: {}".format(filename))
-    data = {  }
+    data = { 'comments' : 0,
+             'classes' : [],
+             'functions' : [],
+             'recipes' : [],
+             'state' : [],
+             'events' : []
+             }
     lines = []
-    with open(filename,'r') as f:
-        lines = f.readlines()
+    with open(filename,'rb') as f:
+        lines = [x.decode('utf-8','ignore') for x in f.readlines()]
 
     state = { 'bracket_count' : 0,
               'current' : None,
@@ -69,15 +132,21 @@ def extract_from_file(filename, main_parser, com_parser):
         state['line'] += 1
         current = lines.pop(0)
 
-        try:
-            comment = com_parser.parseString(current)
+
+        result = main_parser.parseString(current)[0]
+
+        if isinstance(result, utils.ParseBase):
+            result._line_no = state['line']
+
+        if isinstance(result, lua_e) and result is lua_e.COMMENT:
             data['comments'] += 1
-        except pp.ParseException:
-            result = main_parser.parseString(current)
-            #Get open and close brackets
-            #handle result:
-            if not result:
-                continue
+        elif isinstance(result, LuaClass):
+            data['classes'].append(result)
+        elif isinstance(result, LuaFn):
+            data['functions'].append(result)
+        elif isinstance(result, LuaRecipe):
+            data['recipes'].append(result)
+
 
     return data
 
@@ -93,8 +162,8 @@ if __name__ == "__main__":
     else:
         files = utils.get_data_files([join("data","lua")], ".lua")
 
-    mp, cp = build_parser()
+    mp = build_parser()
     for f in files:
-        data = extract_from_file(f, mp, cp)
-        data_str = utils.convert_data_to_output_format(data, [])
+        data = extract_from_file(f, mp)
+        data_str = utils.convert_data_to_output_format(data, ["classes", "functions","recipes"])
         utils.write_output(f, data_str, ".lua_analysis")
