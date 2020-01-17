e90d207220aa602788b15c74faf2ddd3f6378ed2
John Grey <johngrey4296@gmail.com>
2020-01-17 00:37:16 +0000

    add accumulation counts to xml_rule_extractor

--- python/code_analysis/xml_rule_extractor.py
+++ python/code_analysis/xml_rule_extractor.py
@@ -65,6 +65,9 @@ def extract_from_cif_library(soup):
 
     data = utils.xml_search_components(data, soup, data['cif_library_contents'])
 
+    cif_library_components = {'responderinfluenceruleset', 'initiatorinfluenceruleset', 'performancerealization', 'name', 'conditionalrules', 'conditionrule', 'effect', 'instantiations', 'definition', 'instantiation', 'rule', 'influencerule', 'preconditions', 'changerule', 'toc2', 'partialchange', 'toc3', 'lineofdialogue', 'patsyrule', 'intents', 'effects', 'toc1', 'microtheory', 'chorusrule', 'predicate', 'socialgame'}
+
+    data['all_counts'] = {x : len(soup.find_all(x)) for x in cif_library_components}
 
     return data
 
@@ -74,20 +77,71 @@ def extract_from_cifstate(soup):
     data['cif_state_contents'] = list({x.name for x in contents if x.name is not None})
 
     data = utils.xml_search_components(data, soup, data['cif_state_contents'])
+    cif_state_components = {'trait', 'status', 'conditionrule', 'rule', 'relationship', 'character', 'backstorycontext', 'performancerealization', 'edge', 'proposition', 'changerule', 'predicate', 'locution', 'trigger'}
+
+    data['all_counts'] = {x : len(soup.find_all(x)) for x in cif_state_components}
 
 
     return data
 
 def extract_from_promweek(soup):
-    data = { 'toplevel_components' : [] }
+    data = { }
     contents = soup.find('promweek')
-    data['prom_week_coontents'] = list({x.name for x in contents if x.name is not None})
+    data['prom_week_contents'] = list({x.name for x in contents if x.name is not None})
 
     data = utils.xml_search_components(data, soup, data['prom_week_contents'])
+    prom_week_components = {'endings', 'todorule', 'forcedsgs', 'todoitems', 'todolist', 'conditionalrules', 'tidbit', 'quickplayendingdescription', 'cast', 'goalrules', 'description', 'tasknaturallanguage', 'instantiations', 'levels', 'rule', 'goaldescription', 'instantiation', 'todoitem', 'preconditions', 'level', 'toc2', 'partialchange', 'setting', 'forcedsg', 'toc3', 'lineofdialogue', 'condition', 'toc1', 'chorusrule', 'charactername', 'predicate', 'quickplaydescription', 'ending'}
+
+    data['all_counts'] = {x : len(soup.find_all(x)) for x in prom_week_components}
+
 
     return data
 
 
+
+def accumulator(new_data, acc_data):
+    #accumulator descriptions for cifstates, promweeks and ciflibraries
+
+    if 'is_cifstate' in new_data:
+        app_keys = [x for x in new_data.keys() if '_components' in x]
+        acc_data['_cif_state_components'].update([y for x in app_keys for y in new_data[x]])
+        for x in new_data['all_counts'].keys():
+            if x not in acc_data['_cif_state_counts']:
+                acc_data['_cif_state_counts'][x] = 0
+            if x not in acc_data['__all_counts']:
+                acc_data['__all_counts'][x] = 0
+            acc_data['__all_counts'][x] += new_data['all_counts'][x]
+            acc_data['_cif_state_counts'][x] += new_data['all_counts'][x]
+
+
+    elif 'is_promweek' in new_data:
+        app_keys = [x for x in new_data.keys() if '_components' in x]
+        acc_data['_prom_week_components'].update([y for x in app_keys for y in new_data[x]])
+        for x in new_data['all_counts'].keys():
+            if x not in acc_data['_prom_week_counts']:
+                acc_data['_prom_week_counts'][x] = 0
+            if x not in acc_data['__all_counts']:
+                acc_data['__all_counts'][x] = 0
+            acc_data['__all_counts'][x] += new_data['all_counts'][x]
+            acc_data['_prom_week_counts'][x] += new_data['all_counts'][x]
+
+    elif 'is_cif_library' in new_data:
+        app_keys = [x for x in new_data.keys() if '_components' in x]
+        acc_data['_cif_library_components'].update([y for x in app_keys for y in new_data[x]])
+        for x in new_data['all_counts'].keys():
+            if x not in acc_data['_cif_library_counts']:
+                acc_data['_cif_library_counts'][x] = 0
+            if x not in acc_data['__all_counts']:
+                acc_data['__all_counts'][x] = 0
+            acc_data['__all_counts'][x] += new_data['all_counts'][x]
+            acc_data['_cif_library_counts'][x] += new_data['all_counts'][x]
+
+    return acc_data
+
+def accum_final(data):
+
+
+    return data
 
 if __name__ == "__main__":
     queue = join("data","xml","CiFStates")
@@ -95,8 +149,21 @@ if __name__ == "__main__":
     output_lists = []
     output_ext = ".xml_rule_analysis"
 
+    initial_accum = {'_cif_state_components': set(),
+                     '_prom_week_components' : set(),
+                     '_cif_library_components' : set(),
+
+                     '_cif_state_counts': {},
+                     '_prom_week_counts' : {},
+                     '_cif_library_counts' : {},
+                     '__all_counts' : {}
+    }
+
     utils.standard_main(queue,
                         input_ext,
                         extract_from_file,
                         output_lists,
-                        output_ext)
+                        output_ext,
+                        accumulator=accumulator,
+                        accumulator_final=accum_final,
+                        init_accum=initial_accum)
